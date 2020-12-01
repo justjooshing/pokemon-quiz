9219abfd012c1670998477d64b342e6642e368f6
Joshua Mertens <joshua.l.mertens@gmail.com>
2020-12-01 11:31:54 +0000

    add logic/text for easy/hard mode on final page

--- src/public/js/components/final_page/final_page_text/FinalPageText.jsx
+++ src/public/js/components/final_page/final_page_text/FinalPageText.jsx
@@ -3,19 +3,30 @@ import React, { Component } from 'react'
 export class FinalPageText extends Component {
 
   state = {
-    under_four: ["Oh no!", "You're going to have to train a lot more to become the best"] ,
-    under_seven: ["Close!", "You know your stuff, but still need some training"],
-    seven_and_up: ["Well done!", "You are truly on your way to becoming a pokemon master"]
+    easy: {
+      under_four: ["Oh no!", "You're going to have to train a lot more to become the best"] ,
+      under_seven: ["Close!", "You know your stuff, but still need some training"],
+      seven_and_up: ["Well done!", "You are truly on your way to becoming a pokemon master"]
+      },
+    hard: {
+      under_four: ["Oh no!", "You've been training, but are not at pokemon master level just yet"] ,
+      under_seven: ["Close!", "Gary is still ahead of you on the journey to become a pokemon master"],
+      seven_and_up: ["Well done!", "You truly are a pokemon master"]
+    }
   }
 
   render() {
     const {score, setMode} = this.props;
     return (
       <div>
-        <p className="final_page_text_paragraph">{score <4 ? this.state.under_four[0] : score <7 ? this.state.under_seven[0] :
-            this.state.seven_and_up[0]}</p>
-        <p className="final_page_text_paragraph">{score <4 ? this.state.under_four[1] : score <7 ? this.state.under_seven[1] :
-            this.state.seven_and_up[1]}</p>
+        <p className="final_page_text_paragraph">{score <4 ? this.state[setMode].under_four[0] : score <7 ? this.state[setMode].under_seven[0] :
+            this.state[setMode].seven_and_up[0]}</p>
+        <p className="final_page_text_paragraph">
+          { score <4 ? this.state[setMode].under_four[1] : 
+            score <7 ? this.state[setMode].under_seven[1] :
+            this.state[setMode].seven_and_up[1]
+          }
+        </p>
       </div>
     )
   }
